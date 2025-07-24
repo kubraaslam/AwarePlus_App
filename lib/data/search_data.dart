4d2e233ab91c03346f71bd5b629d70c012f0e484
Kubra Aslam <cb012428@students.apiit.lk>
2025-07-24 16:22:53 +0530

    search text field functionality

---
+++ lib/data/search_data.dart
@@ -0,0 +1,10 @@
+final List<Map<String, String>> searchItems = [
+  {'title': 'Knowledge', 'route': '/knowledge'},
+  {'title': 'Sexual and Reproductive Health Education', 'route': '/knowledge'},
+  {'title': 'Physical Sexual Health', 'route': '/knowledge'},
+  {'title': 'Rights, Laws & Ethics', 'route': '/knowledge'},
+  {'title': 'Myths & Misconceptions', 'route': '/knowledge'},
+  {'title': 'Support Services', 'route': '/support'},
+  {'title': 'Glossary', 'route': '/glossary'},
+  {'title': 'Emergency Contacts', 'route': '/emergency'},
+];
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:aware_plus/data/search_data.dart';
 import 'package:aware_plus/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,8 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   int _currentIndex = 0;
+  String _searchQuery = '';
+  List<Map<String, String>> _filteredItems = [];
 
   void _onTabTapped(int index) {
     if (_currentIndex == index) return;
@@ -34,6 +37,13 @@ class _HomeViewState extends State<HomeView> {
     }
   }
 
+  final FocusNode _searchFocusNode = FocusNode();
+  @override
+  void dispose() {
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,105 +52,135 @@ class _HomeViewState extends State<HomeView> {
         currentIndex: _currentIndex,
         onTap: _onTabTapped,
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/img/awareplus-logo.png',
-                height: 100,
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  prefixIcon: Icon(Icons.search),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/knowledge');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 231, 99, 110),
-                  minimumSize: Size(double.infinity, 100),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                child: const Text(
-                  'Knowledge',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/support');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 231, 99, 110),
-                  minimumSize: Size(double.infinity, 100),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                child: const Text(
-                  'Support Services',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 40),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Quick Links',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/faqs');
+      body: GestureDetector(
+        onTap: ()  {
+          FocusScope.of(context).unfocus(); // dismiss keyboard
+          setState(() {
+            _filteredItems.clear(); // hide search list
+          });
+        },
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/img/awareplus-logo.png', height: 100),
+                const SizedBox(height: 20),
+                TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _searchQuery = value.toLowerCase();
+                      _filteredItems =
+                          searchItems.where((item) {
+                            return item['title']!.toLowerCase().contains(
+                              _searchQuery,
+                            );
+                          }).toList();
+                    });
                   },
-                  child: Text(
-                    'FAQs',
-                    style: TextStyle(
-                      color: Colors.black,
-                      decoration: TextDecoration.underline,
-                      fontSize: 15,
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    prefixIcon: Icon(Icons.search),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
                     ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
+                const SizedBox(height: 10),
+                if (_filteredItems.isNotEmpty)
+                  Column(
+                    children:
+                        _filteredItems.map((item) {
+                          return ListTile(
+                            title: Text(item['title']!),
+                            leading: Icon(Icons.arrow_forward_ios, size: 16),
+                            onTap: () {
+                              Navigator.pushNamed(context, item['route']!);
+                            },
+                          );
+                        }).toList(),
+                  ),
+                const SizedBox(height: 30),
+                ElevatedButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, '/emergency');
+                    Navigator.pushNamed(context, '/knowledge');
                   },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 231, 99, 110),
+                    minimumSize: Size(double.infinity, 120),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'Knowledge',
+                    style: TextStyle(fontSize: 22, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/support');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 231, 99, 110),
+                    minimumSize: Size(double.infinity, 120),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'Support Services',
+                    style: TextStyle(fontSize: 22, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const Align(
+                  alignment: Alignment.centerLeft,
                   child: Text(
-                    'Emergency Contacts',
-                    style: TextStyle(
-                      color: Colors.black,
-                      decoration: TextDecoration.underline,
-                      fontSize: 15,
+                    'Quick Links',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/emergency');
+                    },
+                    child: Text(
+                      'Emergency Contacts',
+                      style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.underline,
+                        fontSize: 15,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/glossary');
+                    },
+                    child: Text(
+                      'Glossary',
+                      style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.underline,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
